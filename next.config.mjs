49a32141388ca7cca062181c2b8e181543bfda08
deepsalunkhee <deepsalunkhee@gmail.com>
2024-05-17 16:54:44 +0530

    added the intro part

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+import withVideos from 'next-videos'
 const nextConfig = {};
 
-export default nextConfig;
+
+
+export default {
+    ...nextConfig,
+    ...withVideos(),
+};
+
+
---
+++ src/app/components/terminal/page.tsx
@@ -0,0 +1,17 @@
+
+
+const Terminal=()=>{
+    return(
+        <div className="p-2">
+           <span className="rootuser">
+                <span className="text-green-500">hacker@kali</span>
+                <span className="text-white">:</span>
+                <span className="text-blue-500">~</span>
+           </span>
+        </div>
+        //greating message
+        
+    )
+}
+
+export default Terminal;
